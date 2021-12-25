6fc4231a3006e8c85df43d3622b59ba13597141e
Jaehan Park <38485612+terria1020@users.noreply.github.com>
2021-12-25 10:33:19 +0900

    reset button modified, clear button features not with localStorage
    리셋 버튼 리스너의 올바르지 못한 매칭을 바로잡고, todo clear button의 기능을 추가하였습니다.
    todo clear은 todo가 아직 localstorage를 사용하지 않는 관계로 html에서만 삭제되도록 설계되었습니다.

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    const resetBtn = document.addEventListener("click", (event) => {
+    const resetBtn = document.querySelector("#reset_btn");
+    resetBtn.addEventListener("click", (event) => {
         localStorage.removeItem("name");
+    });
+
+    const todoClearBtn = document.querySelector("#todo_clear_btn");
+    todoClearBtn.addEventListener("click", (event) => {
+        const todos = document
+            .querySelector("#todo_list")
+            .querySelectorAll("li");
+
+        todos.forEach((element) => {
+            element.parentNode.removeChild(element);
+        });
     });
 });
