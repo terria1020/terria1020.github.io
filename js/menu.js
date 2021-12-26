55849f5046ca801b326805a487c81ea86506616a
Jaehan Park <38485612+terria1020@users.noreply.github.com>
2021-12-26 23:57:44 +0900

    added nextQuote button, nextQuote features
    새로운 명언을 띄울 수 있는 버튼을 추가하였습니다.
    newQuote 버튼의 기능을 구현하였습니다.
    
    menu.js에서 DOMContentLoaded 이벤트 리스너의 내용 중 변수 선언이 먼저 나열되게 수정하였습니다.

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,23 @@
+function setNextQuote(jsondata) {
+    const quote = document.querySelector("#quote");
+    const quoteAuthor = document.querySelector("#quote_author");
+
+    const jsonArray = Object.values(jsondata)[0];
+    const randNum = Math.floor(Math.random() * jsonArray.length);
+
+    quote.innerText = `"${jsonArray[randNum].quote}"`;
+    quoteAuthor.innerText = `- ${jsonArray[randNum].author} -`;
+}
+
 document.addEventListener("DOMContentLoaded", (event) => {
     const resetBtn = document.querySelector("#reset_btn");
+    const todoClearBtn = document.querySelector("#todo_clear_btn");
+    const nextQuoteBtn = document.querySelector("#next_quote_btn");
+
     resetBtn.addEventListener("click", (event) => {
         localStorage.removeItem("name");
     });
 
-    const todoClearBtn = document.querySelector("#todo_clear_btn");
     todoClearBtn.addEventListener("click", (event) => {
         const todos = document
             .querySelector("#todo_list")
@@ -13,5 +26,11 @@ document.addEventListener("DOMContentLoaded", (event) => {
         todos.forEach((element) => {
             element.parentNode.removeChild(element);
         });
+    });
+
+    nextQuoteBtn.addEventListener("click", (event) => {
+        fetch("../data/quotes.json")
+            .then((response) => response.json())
+            .then(setNextQuote);
     });
 });
