6e74527d93d85e756b240327723d84490a821861
Jaehan Park <38485612+terria1020@users.noreply.github.com>
2021-12-27 00:59:13 +0900

    added special number for idenfify li, li with id function features
    localStorage 사용 시 item 삭제를 위해 id를 부여할 수 있게 하였습니다.

--- js/todo.js
+++ js/todo.js
@@ -1,12 +1,62 @@
-function loadTodos() {
-    return localStorage.getItem("todos");
+function isLoggedIn() {
+    const name = localStorage.getItem("name");
+    return name !== null;
 }
 
-function saveTodos() {}
+function getQualifiedNum() {
+    return localStorage.getItem("qualifiedNum");
+}
+
+function addQualifiedNum() {
+    let num = Number(getQualifiedNum());
+    num += 1;
+    localStorage.setItem("qualifiedNum", num);
+}
+
+function loadTodos() {
+    const todos = localStorage.getItem("todos");
+    if (todos === null) return [];
+    else return todos;
+}
+
+function attachNewListItem(text) {
+    const todoList = document.querySelector("#todo_list");
+
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+
+    span.innerText = text;
+    button.innerText = "X";
+
+    button.addEventListener("click", (event) => {
+        console.log(event);
+        console.log(event.currentTarget.parentNode);
+    });
+
+    li.id = `item${getQualifiedNum()}`;
+    addQualifiedNum();
+
+    li.appendChild(span);
+    li.appendChild(button);
+
+    todoList.appendChild(li);
+}
+
+function saveTodos(text) {}
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    const todoForm = document.querySelector("#todo_form");
-    todoForm.addEventListener("submit", (event) => {
-        event.preventDefault();
-    });
+    if (isLoggedIn()) {
+        if (!getQualifiedNum()) {
+            localStorage.setItem("qualifiedNum", 0);
+        }
+        const todoForm = document.querySelector("#todo_form");
+
+        todoForm.addEventListener("submit", (event) => {
+            event.preventDefault();
+            attachNewListItem("hello");
+        });
+
+        const savedTodos = loadTodos();
+    }
 });
