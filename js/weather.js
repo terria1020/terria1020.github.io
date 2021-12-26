851c6f8bde135da09eceb80f237bb27b72685f5b
Jaehan Park <38485612+terria1020@users.noreply.github.com>
2021-12-27 02:47:52 +0900

    weather implements features
    사용자의 위치를 받아서 현재 지역, 온도, 날씨를 알려주는 기능을 구현하였습니다.

---
+++ js/weather.js
@@ -0,0 +1,27 @@
+function wellDone(pos) {
+    const weather = document.querySelector("#location_weather");
+    const temp = document.querySelector("#temp");
+
+    const lat = pos.coords.latitude;
+    const lon = pos.coords.longitude;
+
+    const API_KEY = "85e1fa6906e3d3a1820da52878b7b5e5";
+
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            weather.innerText = `지역: ${data.name}, 날씨: ${data.weather[0].main}`;
+            temp.innerText = `현재 온도: ${data.main.temp}°C, 체감 온도: ${data.main.feels_like}°C`;
+        });
+}
+
+function notWork() {
+    const weather = document.querySelector("#location_weather");
+    weather.innerText = "[!] can't loaded your pos.";
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    navigator.geolocation.getCurrentPosition(wellDone, notWork);
+});
